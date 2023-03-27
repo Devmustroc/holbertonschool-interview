0d68239eb1757de4fff1317106a564c9feab4513
Mustapha Abourar <elmustapha.abourar@outlook.com>
2023-03-27 10:36:46 +0200

    lockboxes

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -7,18 +7,18 @@ Method that determines if all the boxes can be opened.
 
 def canUnlockAll(boxes):
     """Unlock array of boxes of keys with indices"""
-    idx = 0  # Start with the first box (index 0)
-    size = len(boxes)  # Get the number of boxes in the input array
-    check = {}  # Create an empty dictionary to keep track of which boxes can be opened
+    idx = 0
+    size = len(boxes)
+    check = {}
 
-    for ks in boxes:  # Loop through each box in the input array
-        if len(ks) == 0 or idx == 0:  # If the box is empty or it's the first box (which is already open)
-            check[idx] = -1  # Mark the box as empty in the checker dictionary (-1 means empty)
-        for key in ks:  # Loop through each key in the current box
-            if key < size and key != idx:  # If the key is valid (i.e. it corresponds to a box in the input array), and it doesn't open the current box
-                check[key] = key  # Mark the corresponding box as open in the checker dictionary
-        if len(check) == size:  # If all boxes have been marked as open in the checker dictionary
-            return True  # Return True (i.e. all boxes can be opened)
-        idx += 1  # Move on to the next box
+    for ks in boxes:
+        if len(ks) == 0 or idx == 0:
+            check[idx] = -1
+        for key in ks:
+            if key < size and key != idx:
+                check[key] = key
+        if len(check) == size:
+            return True
+        idx += 1
 
-    return False  # If we reach this point, we were not able to open all boxes, so return False
+    return False
