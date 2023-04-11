acf59e422bfb761fdd8ee69f0cfe3ac7463dc6ad
Mustapha Abourar <elmustapha.abourar@outlook.com>
2023-04-11 12:34:58 +0200

    min Operations

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -9,14 +9,18 @@ Returns:
 
 
 def minOperations(n):
+    # If n is less than or equal to 1, return 0
     if n <= 1:
         return 0
+    # Initialize "i" to 2 and count to 0
     i = 2
+    # While "i" is less than or equal to n
     count = 0
-    while i <= n:
-        if n % i == 0:
-            n /= i
-            count += i
+
+    while i <= n:  # while "i" is less than or equal to n
+        if n % i == 0:  # if n is divisible by "i"
+            n /= i  # n = n / "i"
+            count += i  # count = count + "i"
         else:
             i += 1
-    return count
+    return count  # return count
