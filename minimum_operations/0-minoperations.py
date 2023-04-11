c1dc32774e6313b6efb7a6822052bcb44c0f4620
Mustapha Abourar <elmustapha.abourar@outlook.com>
2023-04-11 12:22:00 +0200

    min Operations

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 """
-        Calculates the sum of prime factors of a given positive integer.
-
-        Args:
-        n (int): A positive integer to find the sum of prime factors of.
-
-        Returns:
-        int: The sum of prime factors of the given integer.
-    """
+Calculates the sum of prime factors of a given positive integer.
+Args:
+    n (int): A positive integer to find the sum of prime factors of.
+Returns:
+    int: The sum of prime factors of the given integer.
+"""
 
 def minOperations(n):
-
     if n <= 1:
         return 0
     i = 2
