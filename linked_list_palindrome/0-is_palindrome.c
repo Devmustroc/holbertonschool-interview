7326a165f1e0e378dd918e406e2493ecc489a349
Mustapha Abourar <101810866+Devmustroc@users.noreply.github.com>
2023-04-25 16:42:24 +0200

    betty Fix

--- linked_list_palindrome/0-is_palindrome.c
+++ linked_list_palindrome/0-is_palindrome.c
@@ -9,30 +9,33 @@
  */
 int is_palindrome(listint_t **head)
 {
-    listint_t *slow = *head, *fast = *head; // slow and fast pointers
-    listint_t *stack = NULL, *tmp; // stack and tmp pointers
+	listint_t *slow = *head, *fast = *head; /* slow and fast pointers */
+	listint_t *stack = NULL, *tmp; /* stack and tmp pointers */
 
-    // Find the middle of the list using the slow and fast pointers
-    while (fast && fast->next) { // while fast and fast->next are not NULL
-        fast = fast->next->next; // move fast pointer two nodes at a time
-        tmp = slow; // save the current node
-        slow = slow->next; // move to the next node
-        tmp->next = stack; // push the nodes before the middle onto the stack
-        stack = tmp;  // push the nodes before the middle onto the stack
-    }
-
-    if (fast) {  // odd number of nodes, skip the middle node
-        slow = slow->next; // move to the next node
-    }
-
-    // Compare the second half of the list with the nodes on the stack
-    while (slow && stack) {
-        if (slow->n != stack->n) {
-            return 0;  // not a palindrome
-        }
-        slow = slow->next; // move to the next node
-        stack = stack->next; // pop the node from the stack
-    }
-
-    return 1;  // palindrome
+	/* Find the middle of the list using the slow and fast pointers */
+	while (fast && fast->next)
+	{
+		/*  while fast and fast->next are not NULL */
+		fast = fast->next->next; /* move fast pointer two nodes at a time */
+		tmp = slow; /* save the current node */
+		slow = slow->next; /* move to the next node */
+		tmp->next = stack; /* push the nodes before the middle onto the stack */
+		stack = tmp;  /* push the nodes before the middle onto the stack */
+	}
+	if (fast)
+	{
+		/* odd number of nodes, skip the middle node */
+		slow = slow->next; /* move to the next node */
+	}
+	/* Compare the second half of the list with the nodes on the stack */
+	while (slow && stack)
+	{
+		if (slow->n != stack->n)
+		{
+			return (0);  /* not a palindrome */
+		}
+		slow = slow->next; /* move to the next node */
+		stack = stack->next; /* pop the node from the stack */
+	}
+	return (1);  /* palindrome */
 }
