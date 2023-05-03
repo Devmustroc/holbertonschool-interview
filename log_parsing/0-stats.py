c683540869677f7f8d19d94c3a4e5809ac1663ab
Mustapha Abourar <101810866+Devmustroc@users.noreply.github.com>
2023-05-03 13:14:04 +0200

    Sandpiles

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 """
-Module that parses a log and prints stats to stdout
-
-Usage:
-    ./0-generator.py | ./0-stats.py
+script that reads stdin line by line and computes metrics
 """
 import sys
 
