a77549c6c1d7a8398f6e9963a3c870f221a89e8f
Mustapha Abourar <101810866+Devmustroc@users.noreply.github.com>
2023-05-03 13:36:10 +0200

    Log Parsing

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -4,17 +4,45 @@ Module that parses a log and prints stats to stdout
 """
 import sys
 
-status_codes = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0,
-}
 
-size = 0
+if __name__ == "__main__":
+    st_code = {"200": 0,
+               "301": 0,
+               "400": 0,
+               "401": 0,
+               "403": 0,
+               "404": 0,
+               "405": 0,
+               "500": 0}
+    count = 1
+    file_size = 0
 
+    def parse_line(line):
+        """ Read, parse and grab data"""
+        try:
+            parsed_line = line.split()
+            status_code = parsed_line[-2]
+            if status_code in st_code.keys():
+                st_code[status_code] += 1
+            return int(parsed_line[-1])
+        except Exception:
+            return 0
+
+    def print_stats():
+        """print stats in ascending order"""
+        print(f"File size: {file_size}")
+        for key in sorted(st_code.keys()):
+            if st_code[key]:
+                print(f"{key}: {st_code[key]}")
+
+    try:
+        for line in sys.stdin:
+            file_size += parse_line(line)
+            if count % 10 == 0:
+                print_stats()
+            count += 1
+    except KeyboardInterrupt:
+        print_stats()
+        raise
+    print_stats()
 
