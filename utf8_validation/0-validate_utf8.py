abf032b8224b86a1982c5d0057d8b802bbd02810
Devmustroc <elmustapha.abourar@outlook.com>
2023-05-24 20:38:51 +0200

    utf-8

---
+++ utf8_validation/0-validate_utf8.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""UTF-8 Validation"""
+
+
+def validUTF8(data):
+    num_bytes = 0  # Number of bytes in the current character
+
+    for num in data:
+        # Check if the current byte is a continuation byte
+        if num & 0b11000000 == 0b10000000:
+            if num_bytes == 0:
+                return False  # Invalid continuation byte without a leading byte
+            num_bytes -= 1
+        else:
+            if num_bytes != 0:
+                return False  # Incomplete character
+            if num & 0b10000000 == 0:
+                num_bytes = 0  # ASCII character
+            elif num & 0b11100000 == 0b11000000:
+                num_bytes = 1  # Character with 2 bytes
+            elif num & 0b11110000 == 0b11100000:
+                num_bytes = 2  # Character with 3 bytes
+            elif num & 0b11111000 == 0b11110000:
+                num_bytes = 3  # Character with 4 bytes
+            else:
+                return False  # Invalid leading byte
+
+    return num_bytes == 0
