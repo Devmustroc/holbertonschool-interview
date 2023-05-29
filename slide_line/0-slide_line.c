e893d51820a9b27f468a10a7262d8f360578df9b
Devmustroc <elmustapha.abourar@outlook.com>
2023-05-29 20:46:51 +0200

    2048 comments

--- slide_line/0-slide_line.c
+++ slide_line/0-slide_line.c
@@ -7,32 +7,29 @@
  */
 void slide_left(int *line, size_t size)
 {
-	size_t i, j;
-	int merged[size];
-	int merge_index = 0;
+    size_t i, j;
+    int merged[32] = {0};
+    int merge_index = 0;
 
-	for (i = 0; i < size; i++)
-		merged[i] = 0;
+    for (i = 0; i < size; i++)
+    {
+        if (line[i] != 0)
+        {
+            if (merge_index > 0 && line[i] == merged[merge_index - 1])
+            {
+                line[i] *= 2;
+                merged[merge_index - 1] = line[i];
+            }
+            else
+            {
+                line[merge_index] = line[i];
+                merge_index++;
+            }
+        }
+    }
 
-	for (i = 0; i < size; i++)
-	{
-		if (line[i] != 0)
-		{
-			if (merge_index > 0 && line[i] == merged[merge_index - 1])
-			{
-				line[i] *= 2;
-				merged[merge_index - 1] = line[i];
-			}
-			else
-			{
-				line[merge_index] = line[i];
-				merge_index++;
-			}
-		}
-	}
-
-	for (j = merge_index; j < size; j++)
-		line[j] = 0;
+    for (j = merge_index; j < size; j++)
+        line[j] = 0;
 }
 /**
  * slide_right - Slide and merge the line to the right
@@ -42,16 +39,16 @@ void slide_left(int *line, size_t size)
  */
 void slide_right(int *line, size_t size)
 {
-	size_t i;
-	int temp[size];
+    size_t i;
+    int temp[32] = {0};
 
-	for (i = 0; i < size; i++)
-		temp[i] = line[size - 1 - i];
+    for (i = 0; i < size; i++)
+        temp[i] = line[size - 1 - i];
 
-	slide_left(temp, size);
+    slide_left(temp, size);
 
-	for (i = 0; i < size; i++)
-		line[i] = temp[size - 1 - i];
+    for (i = 0; i < size; i++)
+        line[i] = temp[size - 1 - i];
 }
 /**
  * slide_line - Slide and merge an array of integers
@@ -64,12 +61,13 @@ void slide_right(int *line, size_t size)
  */
 int slide_line(int *line, size_t size, int direction)
 {
-	if (line == NULL || (direction != SLIDE_LEFT && direction != SLIDE_RIGHT))
-		return (0);
+    if (line == NULL || (direction != SLIDE_LEFT && direction != SLIDE_RIGHT))
+        return 0;
 
-	if (direction == SLIDE_LEFT)
-		slide_left(line, size);
-	else
-		slide_right(line, size);
-	return (1);
+    if (direction == SLIDE_LEFT)
+        slide_left(line, size);
+    else
+        slide_right(line, size);
+
+    return 1;
 }
