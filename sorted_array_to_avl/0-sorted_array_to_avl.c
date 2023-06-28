f373ad5b7361558a67173e213dd060335dd74434
Devmustroc <elmustapha.abourar@outlook.com>
2023-06-29 01:29:13 +1200

    AVL betty

--- sorted_array_to_avl/0-sorted_array_to_avl.c
+++ sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -4,24 +4,26 @@
  * @array: pointer to the first element of the array to be converted
  * @start: start index of the subarray
  * @end: end index of the subarray
- * @parent: pointer to the parent node
+ * @par: pointer to the parent node
  * Return: pointer to the root node of the created AVL tree, or NULL on failure
  */
-avl_t *sorted_array_to_avl_helper(int *array, int start, int end, avl_t *parent)
+avl_t *sorted_array_to_avl_helper(int *array, int start, int end, avl_t *par)
 {
-    avl_t *new_node;
-    int mid;
-    if (start > end)
-        return NULL;
-    mid = (start + end) / 2;
-    new_node = malloc(sizeof(avl_t));
-    if (new_node == NULL)
-        return NULL;
-    new_node->n = array[mid];
-    new_node->parent = parent;
-    new_node->left = sorted_array_to_avl_helper(array, start, mid - 1, new_node);
-    new_node->right = sorted_array_to_avl_helper(array, mid + 1, end, new_node);
-    return new_node;
+	avl_t *new_node;
+	int mid;
+
+	if (start > end)
+		return (NULL);
+
+	mid = (start + end) / 2;
+	new_node = malloc(sizeof(avl_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = array[mid];
+	new_node->parent = parent;
+	new_node->left = sorted_array_to_avl_helper(array, start, mid - 1, new_node);
+	new_node->right = sorted_array_to_avl_helper(array, mid + 1, end, new_node);
+	return (new_node);
 }
 /**
  * sorted_array_to_avl - builds an AVL tree from an array
@@ -31,7 +33,7 @@ avl_t *sorted_array_to_avl_helper(int *array, int start, int end, avl_t *parent)
  */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-    if (array == NULL || size == 0)
-        return NULL;
-    return sorted_array_to_avl_helper(array, 0, size - 1, NULL);
+	if (array == NULL || size == 0)
+		return (NULL);
+	return (sorted_array_to_avl_helper(array, 0, size - 1, NULL));
 }
