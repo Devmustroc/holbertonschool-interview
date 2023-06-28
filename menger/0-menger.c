fa3e9211622ecd06d37b1a5863708525fba5e3db
Devmustroc <elmustapha.abourar@outlook.com>
2023-06-28 16:31:53 +0200

    AVL Trees

--- menger/0-menger.c
+++ menger/0-menger.c
@@ -1,6 +1,13 @@
 #include "menger.h"
+/**
+ * draw_char - draws a character
+ * @row: row
+ * @col: column
+ * @size: size
+ */
 void draw_char(int row, int col, int size)
 {
+    /* if row or column is 1, print a space */
     for (int i = 0; i < size; i++)
     {
         if (row % 3 == 1 && col % 3 == 1)
@@ -15,12 +22,21 @@ void draw_char(int row, int col, int size)
         col /= 3;
     }
 }
+/**
+ * draw_menger - draws a menger sponge
+ * @level: level
+ * @row: row
+ * @col: column
+ * @size: size
+ */
 void draw_menger(int level, int row, int col, int size)
 {
+    /* if level is 0, print a 1x1 block */
     if (level == 0)
     {
         draw_char(row, col, size);
     }
+    /* if level is greater than 0, print a 3x3 block */
     else
     {
         int newSize = size / 3;
@@ -30,18 +46,26 @@ void draw_menger(int level, int row, int col, int size)
             {
                 int newRow = row + i * newSize;
                 int newCol = col + j * newSize;
+                /* if it's the middle block, print a space */
                 draw_menger(level - 1, newRow, newCol, newSize);
             }
         }
     }
 }
+/**
+ * menger - draws a menger sponge
+ * @level: level
+ */
 void menger(int level)
 {
+    /* if level is less than 0, do nothing */
     if (level < 0)
     {
         return;
     }
+    /* if level is 0, print a 1x1 block */
     int size = pow(3, level);
+    /* if level is 0, print a 1x1 block */
     for (int i = 0; i < size; i++)
     {
         for (int j = 0; j< size; j++)
