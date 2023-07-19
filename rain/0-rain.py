d4f95da01ba4863b923a9e7a5a7994d521aeee53
Devmustroc <elmustapha.abourar@outlook.com>
2023-07-19 11:01:25 +0200

    rain

---
+++ rain/0-rain.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+calculate how many square units of water
+will be retained after it rains.
+"""
+
+
+def rain(walls: list[int]) -> int:
+    """ Rain """
+    if not walls:
+        return 0
+
+    water = 0
+    for i in range(1, len(walls) - 1):
+        left = walls[i]
+        for j in range(i):
+            left = max(left, walls[j])
+        right = walls[i]
+        for j in range(i + 1, len(walls)):
+            right = max(right, walls[j])
+        water += min(left, right) - walls[i]
+    return water
