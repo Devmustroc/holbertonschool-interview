687f989079e6d52b5fa4c870196cf579d3f3849d
devmust <elmustapha.abourar@gmail.com>
2023-07-31 15:24:24 +0200

    Betty Fix Error

--- advanced_binary_search/0-advanced_binary.c
+++ advanced_binary_search/0-advanced_binary.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 /**
- * advanced_binary_recursive - recursive function to search for a value in a sorted array of integers
+ * advanced_binary_recursive - recursive function to search for a value
  * @array: pointer to the first element of the array to search in
  * @left: leftmost index of the subarray
  * @right: rightmost index of the subarray
  * @value: value to search for
  * Return: index where value is located, or -1 if not found
- */
+*/
 int advanced_binary_recursive(int *array, int left, int right, int value)
 {
 	if (left > right)
-		return -1;
+	{
+		return (-1);
+	}
 
 	int mid = (left + right) / 2;
 
@@ -27,17 +29,17 @@ int advanced_binary_recursive(int *array, int left, int right, int value)
 	if (array[mid] == value)
 	{
 		if (mid == left || array[mid - 1] != value)
-			return mid;
+			return (mid);
 		else
-			return advanced_binary_recursive(array, left, mid - 1, value);
+			return (advanced_binary_recursive(array, left, mid - 1, value));
 	}
 	else if (array[mid] < value)
 	{
-		return advanced_binary_recursive(array, mid + 1, right, value);
+		return (advanced_binary_recursive(array, mid + 1, right, value));
 	}
 	else
 	{
-		return advanced_binary_recursive(array, left, mid - 1, value);
+		return (advanced_binary_recursive(array, left, mid - 1, value));
 	}
 }
 /**
@@ -50,7 +52,9 @@ int advanced_binary_recursive(int *array, int left, int right, int value)
 int advanced_binary(int *array, size_t size, int value)
 {
 	if (array == NULL || size == 0)
-		return -1;
+	{
+		return (-1);
+	}
 
-	return advanced_binary_recursive(array, 0, size - 1, value);
+	return (advanced_binary_recursive(array, 0, size - 1, value));
 }
--- advanced_binary_search/search_algos.h
+++ advanced_binary_search/search_algos.h
@@ -1,9 +1,10 @@
-#ifdef _SEARCH_ALGOS_H_
-#define _SEARCH_ALGOS_H_
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <stddef.h>
 
 int advanced_binary(int *array, size_t size, int value);
+int advanced_binary_recursive(int *array, int left, int right, int value);
 
-#endif /* _SEARCH_ALGOS_H_ */
+#endif /* SEARCH_ALGOS_H */
+
