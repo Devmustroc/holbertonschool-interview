60f71d0e55798eee648b31d00532d5c2fa00b1c5
devmust <elmustapha.abourar@gmail.com>
2023-07-31 15:10:03 +0200

    Betty Fix

--- advanced_binary_search/0-advanced_binary.c
+++ advanced_binary_search/0-advanced_binary.c
@@ -1,32 +1,42 @@
-#include "search_algos.h"
 #include <stdio.h>
-/**
-* advanced_binary - searches for a value in a sorted array of integers.
- * @array: pointer to the first element of the array to search in.
- * @size: number of elements in array.
- * @value: value to search for.
- * Return: index where value is located or -1 if value is not present or array
-*/
-int advanced_binary(int *array, size_t size, int value) {
-    size_t i = 0, j = size - 1, m = 0, k = 0;
-    int tmp = 0;
-    if (array == NULL || size == 0)
-        return (-1);
-    while (i <= j) {
-        printf("Searching in array: ");
-        for (k = i; k < j; k++)
-            printf("%d, ", array[k]);
-        printf("%d\n", array[k]);
-        m = (i + j) / 2;
-        if (array[m] < value)
-            i = m + 1;
-        else if (array[m] > value)
-            j = m;
-        else {
-            if (m == 0 || array[m - 1] != value)
-                return (m);
-            j = m;
-        }
-    }
-    return (-1);
+
+int advanced_binary_recursive(int *array, int left, int right, int value)
+{
+	if (left > right)
+		return -1;
+
+	int mid = (left + right) / 2;
+
+	printf("Searching in array: ");
+	for (int i = left; i <= right; i++)
+	{
+		printf("%d", array[i]);
+
+		if (i != right)
+			printf(", ");
+	}
+	printf("\n");
+
+	if (array[mid] == value)
+	{
+		if (mid == left || array[mid - 1] != value)
+			return mid;
+		else
+			return advanced_binary_recursive(array, left, mid - 1, value);
+	}
+	else if (array[mid] < value)
+	{
+		return advanced_binary_recursive(array, mid + 1, right, value);
+	}
+	else
+	{
+		return advanced_binary_recursive(array, left, mid - 1, value);
+	}
+}
+int advanced_binary(int *array, size_t size, int value)
+{
+	if (array == NULL || size == 0)
+		return -1;
+
+	return advanced_binary_recursive(array, 0, size - 1, value);
 }
