3c4496e3eb8c9ea35607ed662aa2d26ddf63749c
Devmustroc <elmustapha.abourar@outlook.com>
2023-08-09 18:58:39 +0200

    Reddit Api

--- count_it/0-count.py
+++ count_it/0-count.py
@@ -1,51 +1,56 @@
-#!/usr/bin/python3
-import requests
+#!/usr/bin/python
+"""Count the number of the Reddit API"""
 
-def parse_titles(posts, word_list, count):
+import request
+
+def parse_titles(posts, word_list, counting):
+    """Parse the titles of the posts"""
     for post in posts:
         title = post['data']['title'].lower()
         for word in word_list:
             word = word.lower()
             n = title.count(word)
-            count[word] = count.get(word, 0) + n
+            counting[word] = counting.get(word, 0) + n
 
-def print_format(count):
-    sorted_count = sorted(count.items(), key=lambda x: (-x[1], x[0]))
-    for k, v in sorted_count:
-        if v > 0:
-            print(f"{k}: {v}")
 
-def count_words(subreddit, word_list, count=None, after=""):
-    if count is None:
-        count = {}
+def print_formatted(count):
+    """Print the count in a formatted way"""
+    if not count:
+        return
 
-    base_url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {"User-Agent": "MyBot"}
-    params = {"after": after}
+    sorted_count = sorted(count.items(), key=lambda x: x[0])
+    sorted_count = sorted(sorted_count, key=lambda x: x[1], reverse=True)
 
-    response = requests.get(base_url, allow_redirects=False, headers=headers, params=params)
+    for key, value in sorted_count:
+        if value == 0:
+            continue
+        print(f"{key}: {value}")
 
-    if response.status_code > 300:
+
+def count_words(subreddit, word_list, count={}, after=""):
+    """
+    Queries the Reddit API, parses the title of all hot articles,
+    and prints a sorted count of given keywords
+    """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'User-agent': 'MyBot'}
+    params = {'after': after}
+
+    result = requests.get(url,
+                          allow_redirects=False,
+                          headers=headers,
+                          params=params)
+
+    if result.status_code > 300:
         return None
 
-    data = response.json().get('data')
+    data = result.json().get('data')
     after = data.get('after')
     posts = data.get('children')
 
     parse_titles(posts, word_list, count)
 
-    if after:
+    if after is not None:
         count_words(subreddit, word_list, count, after)
     else:
-        print_format(count)
-
-if __name__ == '__main__':
-    import sys
-
-    if len(sys.argv) < 3:
-        print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
-        print("Ex: {} programming 'python java javascript'".format(sys.argv[0]))
-    else:
-        subreddit = sys.argv[1]
-        keywords = sys.argv[2:]
-        count_words(subreddit, keywords)
+        print_formatted(count)
