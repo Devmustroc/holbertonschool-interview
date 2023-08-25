21ceb3d6dcdd7a272e00f39c386f9081dfadbbd2
Devmustroc <elmustapha.abourar@outlook.com>
2023-08-25 12:23:39 +0200

    infinite mult

--- infinite_multiplication/0-mul.c
+++ infinite_multiplication/0-mul.c
@@ -4,10 +4,11 @@
 /**
 * is_digit - checks if a string is a digit
  * @c: character to check
+ * Return: 1 if digit, 0 if not
 */
 int is_digit(char c)
 {
-    return (c >= '0' && c <= '9');
+	return (c >= '0' && c <= '9');
 }
 /**
 * errorAndExit - prints Error and exits.
@@ -16,84 +17,88 @@ int is_digit(char c)
 */
 void errorAndExit(void)
 {
-    char error[] = "Error\n";
-    for (int i = 0; error[i] != '\0'; i++)
-    {
-        _putchar(error[i]);
-    }
-    exit(98);
+	char error[] = "Error\n";
+
+	for (int i = 0; error[i] != '\0'; i++)
+	{
+		_putchar(error[i]);
+	}
+	exit(98);
 }
 /**
 * multiply - multiplies two numbers
- * @argc: number of arguments
- * @argv: array of arguments
+ * @num1: first number
+ * @num2: second number
  * Return: 0 on success, 98 on failure
 */
 void multiply(char *num1, char *num2)
 {
-    int len1 = 0, len2 = 0;
+	int len1 = 0, len2 = 0;
 
-    while (num1[len1])
-        len1++;
-    while (num2[len2])
-        len2++;
+	while (num1[len1])
+		len1++;
+	while (num2[len2])
+		len2++;
 
-    int lenResult = len1 + len2;
-    int *result = malloc(sizeof(int) * lenResult);
+	int lenResult = len1 + len2;
+	int *result = malloc(sizeof(int) * lenResult);
 
-    if (!result)
-        errorAndExit();
+	if (!result)
+		errorAndExit();
 
-    for (int i = len1; i >= 0; i--)
-    {
-        for (int j = len2; j >= 0; j--)
-        {
-            int digit1 = (num1[i] - '0') * (num2[j] - '0');
-            int sum = result[i + j + 1] + digit1;
-            result[i + j] += sum / 10;
-            result[i + j + 1] = sum % 10;
-        }
-    }
+	for (int i = len1; i >= 0; i--)
+	{
+		for (int j = len2; j >= 0; j--)
+		{
+			int digit1 = (num1[i] - '0') * (num2[j] - '0');
+			int sum = result[i + j + 1] + digit1;
 
-    int startIndex = 0;
+			result[i + j] += sum / 10;
+			result[i + j + 1] = sum % 10;
+		}
+	}
 
-    while (startIndex < lenResult && result[startIndex] == 0)
-        startIndex++;
+	int startIndex = 0;
 
-    if (startIndex == lenResult)
-        _putchar('0');
-    else
-    {
-        for (int i = startIndex; i < lenResult; i++)
-            _putchar(result[i] + '0');
-    }
-    _putchar('\n');
-    free(result);
+	while (startIndex < lenResult && result[startIndex] == 0)
+		startIndex++;
+
+	if (startIndex == lenResult)
+		_putchar('0');
+	else
+	{
+		for (int i = startIndex; i < lenResult; i++)
+			_putchar(result[i] + '0');
+	}
+	_putchar('\n');
+	free(result);
 }
 /**
-* main - multiplies two numbers
+ * main - multiplies two numbers
  * @argc: number of arguments
  * @argv: array of arguments
+ * Description: multiplies two numbers
  * Return: 0 on success, 98 on failure
 */
 int main(int argc, int *argv[])
 {
-    if (argc != 3)
-        errorAndExit();
+	if (argc != 3)
+		errorAndExit();
 
-    char *num1 = argv[1];
-    char *num2 = argv[2];
+	char *num1 = argv[1];
+	char *num2 = argv[2];
 
-    for (int i = 0; num1[i]; i++)
-    {
-        if (!is_digit(num1[i]))
-            errorAndExit();
-    }
-    for (int i = 0; num2[i]; i++)
-    {
-        if (!is_digit(num2[i]))
-            errorAndExit();
-    }
-    multiply(num1, num2);
-    return (0);
+	for (int i = 0; num1[i]; i++)
+	{
+		if (!is_digit(num1[i]))
+			errorAndExit();
+	}
+	for (int i = 0; num2[i]; i++)
+	{
+		if (!is_digit(num2[i]))
+			errorAndExit();
+	}
+	multiply(num1, num2);
+	return (0);
 }
+
