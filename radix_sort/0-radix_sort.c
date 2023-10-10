ae30fa6d505deb320d139f90f99f83098f77915c
Devmustroc <elmustapha.abourar@outlook.com>
2023-10-10 11:00:17 +0200

    fix c99 problem

--- radix_sort/0-radix_sort.c
+++ radix_sort/0-radix_sort.c
@@ -59,8 +59,9 @@ void radix_sort(int *array, size_t size)
 	if (array == NULL || size < 2)
 		return;
 
-	int max = find_max(array, size);
-	int exp;
+	int max, exp;
+
+	max = find_max(array, size);
 
 	for (exp = 1; max / exp > 0; exp *= 10)
 	{
