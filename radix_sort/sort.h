09ea20708d94e1c8d057dcaa9c1d423603cae1c9
Devmustroc <elmustapha.abourar@outlook.com>
2023-10-20 10:02:21 +0200

    Island Perimeter

--- radix_sort/sort.h
+++ radix_sort/sort.h
@@ -5,8 +5,5 @@
 #include <stdio.h>
 
 void print_array(const int *array, size_t size);
-void counting_sort(int *array, size_t size, int exp);
 void radix_sort(int *array, size_t size);
-int find_max(int *array, size_t size);
-
 #endif
