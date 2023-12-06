4053714d5ba19b8e53b27c71b3095f29f47d2201
devmustroc <elmustapha.abourar@outlook.com>
2023-12-06 11:39:45 +0100

    PrimeGame

--- primegame/0-prime_game.py
+++ primegame/0-prime_game.py
@@ -14,25 +14,26 @@ def isWinner(x, nums):
         return None
     # Assign the max number in the array to n
     n = max(nums)
-    # Create a filter array with True values for each index in the range of n + 1
+    # Create a filter array with True
+    # values for each index in the range of n + 1
     filterArr = [True for _ in range(max(n + 1, 2))]
     # Set the first two values in the filter array to False
     for i in range(2, int(pow(n, 0.5)) + 1):
-        if not filterArr[i]:  # If the value is already False,
-            continue  # continue to the next iteration
-        for j in range(i * i, n + 1, i):  # Set all multiples of i to False
-            filterArr[j] = False  # Set the value to False
-    filterArr[0] = filterArr[1] = False  # Set the first two values to False
+        if not filterArr[i]:
+            continue
+        for j in range(i * i, n + 1, i):
+            filterArr[j] = False
+    filterArr[0] = filterArr[1] = False
     c = 0  # Counter
-    for i in range(len(filterArr)):  # Set the value to the number of primes
+    for i in range(len(filterArr)):
         if filterArr[i]:  # If the value is True
             c += 1  # Increment the counter
         filterArr[i] = c  # Set the value to the counter
     player1 = 0  # Player 1
     for n in nums:  # Iterate through the array
-        player1 += filterArr[n] % 2 == 1  # Increment player 1 if the value is odd
-    if player1 * 2 == len(nums):  # If player 1 is equal to half the length of the array
+        player1 += filterArr[n] % 2 == 1
+    if player1 * 2 == len(nums):
         return None  # Return None
-    if player1 * 2 > len(nums):  # If player 1 is greater than half the length of the array
+    if player1 * 2 > len(nums):
         return "Maria"  # Return Maria
     return "Ben"  # Return Ben
